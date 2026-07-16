92f49c78258648b9d18f9cfe51f37f030d00b431
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-17 03:08:18 +0530

    Time: 35 ms (34.62%), Space: 14 MB (95.16%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for (int j=i+1;j<n;j++){
+                if (nums[i]+nums[j]== target){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
