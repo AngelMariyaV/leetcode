89a43cfb14eb3032d37d5231aa38e00424273cea
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-17 14:51:52 +0530

    Time: 0 ms (100%), Space: 8.9 MB (51.04%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> row(rowIndex + 1, 1);
+        for (int i = 2; i <= rowIndex; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                row[j] = row[j] + row[j - 1];
+            }
+        }
+        
+        return row;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
