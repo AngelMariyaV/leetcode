f2b42de9d979bb3f4774d9de4d49cd7d86231942
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-17 15:41:41 +0530

    Time: 0 ms (100%), Space: 7.7 MB (77.24%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0) return false;
+        while(n > 1)
+        {
+            if (n % 2 == 0) n = n / 2;
+            else if (n % 3 == 0) n = n / 3;
+            else if (n % 5 == 0) n = n / 5;
+            else break;
+        }
+        return (n == 1);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
