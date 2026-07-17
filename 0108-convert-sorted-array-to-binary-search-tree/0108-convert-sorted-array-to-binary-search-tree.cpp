3e7e6477a9e208c28f8b5ecc894d6fc4e0022a03
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-17 14:44:58 +0530

    Time: 0 ms (100%), Space: 22.9 MB (56.21%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* BST(vector<int>& nums, int l, int r){
+        if(l>r){
+            return nullptr;
+        }
+        int mid=l+(r-l)/2;
+        TreeNode* root=new TreeNode(nums[mid]);
+        root->left=BST(nums,l,mid-1);
+        root->right=BST(nums,mid+1,r);
+        return root;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return BST(nums,0,nums.size()-1);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
