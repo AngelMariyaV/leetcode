ce1b4986ea78381c6c3bea26019bbf6d1eff289b
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-17 15:43:55 +0530

    Time: 0 ms (100%), Space: 8 MB (41.8%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,25 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int l=1;
+        int r=n;
+
+        while (l<r){
+            int mid=l+(r-l)/2;
+            if (isBadVersion(mid)){
+                r=mid;
+            }else{
+                l=mid+1;
+            }
+        }
+        return l;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
