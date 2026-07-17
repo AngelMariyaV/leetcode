48bc36a95715f4bcf6ce24bda18b86351014024a
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-17 15:31:58 +0530

    Time: 3 ms (1.74%), Space: 10 MB (32.41%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,46 @@
+class MyStack {
+private:
+    queue<int> q;
+public:
+
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        int sz = q.size();
+        for (int i = 0; i < sz - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int top_element = q.front();
+        q.pop();
+        return top_element;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
