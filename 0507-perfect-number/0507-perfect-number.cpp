7504c72b7310339bc89de1a1a41e601cd1901a86
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-17 16:19:14 +0530

    Time: 1033 ms (55.91%), Space: 7.8 MB (56.69%) - LeetHub - Time: O(num), Space: O(1)

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        
+        int sum=0;
+        for (int i=1;i<=num/2;i++){
+            if (num%i==0){
+                sum+=i;
+            }
+        }
+        return sum==num;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
