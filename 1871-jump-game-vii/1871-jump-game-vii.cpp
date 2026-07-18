54a79cffe99edb7bdb085d08699371cd36e7ea23
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-18 15:13:31 +0530

    Time: 3 ms (94.13%), Space: 26.9 MB (39.39%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+        if (s[n - 1] == '1') return false;
+
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+
+        int reachable = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i >= minJump && dp[i - minJump])
+                reachable++;
+
+            if (i > maxJump && dp[i - maxJump - 1])
+                reachable--;
+
+            if (reachable > 0 && s[i] == '0')
+                dp[i] = 1;
+        }
+
+        return dp[n - 1];
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
