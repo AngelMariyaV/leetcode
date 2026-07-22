167da2e9c36f7f0370250bfbd74e947d7e59130b
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-22 16:31:57 +0530

    Time: 0 ms (100%), Space: 24.3 MB (72.5%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int currVal= 0;
+    int currCount= 0;
+    int maxCount= 0;
+    vector<int> modes;
+
+    void inorder(TreeNode* node) {
+        if (!node) return;
+        inorder(node->left);
+        if (node->val == currVal) {
+            currCount++;
+        } else {
+            currVal = node->val;
+            currCount = 1;
+        }
+
+        if (currCount > maxCount) {
+            maxCount = currCount;
+            modes.clear();
+            modes.push_back(currVal);
+        } else if (currCount == maxCount) {
+            modes.push_back(currVal);
+        }
+        inorder(node->right);
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        if (!root) return {};        
+        currVal = root->val;
+        inorder(root);
+        return modes;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
