4e2ca20254d1779547e1668e8caeea079db224d8
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-24 15:20:20 +0530

    Time: 0 ms (100%), Space: 16.7 MB (70.42%) - LeetHub - Time: O(n*m), Space: O(n*m)

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int count=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1) count++;
+                else if(grid[i][j]==2) q.push({i,j});
+            }
+        }  
+        int time=0;
+        while(!q.empty() && count>0)
+        {
+            int sz=q.size();
+            while(sz--)
+            {
+                pair<int,int>p=q.front();
+                q.pop();
+                int x=p.first;
+                int y=p.second;
+                if(x-1>=0 && grid[x-1][y]==1)
+                {
+                    grid[x-1][y]=2;
+                    count--;
+                    q.push({x-1,y});
+                }
+                if(x+1<n && grid[x+1][y]==1)
+                {
+                    grid[x+1][y]=2;
+                    count--;
+                    q.push({x+1,y});
+                }
+                if(y-1>=0 && grid[x][y-1]==1)
+                {
+                    grid[x][y-1]=2;
+                    count--;
+                    q.push({x,y-1});
+                }
+                if(y+1<m && grid[x][y+1]==1)
+                {
+                    grid[x][y+1]=2;
+                    count--;
+                    q.push({x,y+1});
+                }
+            }
+            time++;
+        } 
+        if(count==0) return time;
+        else return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
