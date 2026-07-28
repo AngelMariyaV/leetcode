1ee2e73e5a96974fbd89d10efdfb4d73872fc042
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-07-28 15:23:54 +0530

    Time: 5 ms (13.23%), Space: 112.3 MB (88.73%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalTank = 0;   
+        int currentTank = 0; 
+        int startStation = 0;
+        for (int i=0; i<gas.size(); ++i) {
+            int netGas = gas[i] - cost[i];
+            totalTank += netGas;
+            currentTank += netGas;
+            if (currentTank < 0) {
+                startStation = i + 1; 
+                currentTank = 0;    
+            }
+        }
+        return (totalTank >= 0) ? startStation : -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
