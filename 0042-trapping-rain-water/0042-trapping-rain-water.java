343d08bb4d098043b0d3c9c29b277d9a7d1ea603
Angel Mariya Sunny <angelmariyasd@gmail.com>
2026-09-24 13:34:45 +0530

    Time: 0 ms (100%), Space: 47.7 MB (53.1%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int trap(int[] height) {
+        int l=0, r=height.length-1;
+        int lMax = 0, rMax = 0;
+        int totalWater = 0;
+
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= lMax) {
+                    lMax = height[l];
+                } else {
+                    totalWater += lMax - height[l];
+                }
+                l++;
+            } else {
+                if (height[r] >= rMax) {
+                    rMax = height[r];
+                } else {
+                    totalWater += rMax - height[r];
+                }
+                r--;
+            }
+        }
+
+        return totalWater;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
